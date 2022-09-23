a32aa99ff3e475c9ced4393f6b137aa97fcd1adb
Bruce0203 <BruceFreedy@gmail.com>
2022-09-24 01:38:53 +0900

    fix: remove describe cron

--- src/main/kotlin/io/github/bruce0203/cronman/Plugin.kt
+++ src/main/kotlin/io/github/bruce0203/cronman/Plugin.kt
@@ -1,13 +1,8 @@
 package io.github.bruce0203.cronman
 
-import com.cronutils.model.CronType.QUARTZ
-import com.cronutils.descriptor.CronDescriptor
-import com.cronutils.model.definition.CronDefinitionBuilder
-import com.cronutils.parser.CronParser
 import it.sauronsoftware.cron4j.Scheduler
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
-import java.util.*
 
 @Suppress("unused")
 class Plugin : JavaPlugin() {
@@ -31,11 +26,6 @@ class Plugin : JavaPlugin() {
                     Bukkit.dispatchCommand(Bukkit.getConsoleSender(), cmd)
                 })
             }
-            val description: String = CronDescriptor.instance(Locale.KOREAN)
-                .describe(
-                    CronParser(CronDefinitionBuilder.instanceDefinitionFor(QUARTZ))
-                        .parse(cronString))
-            logger.info("[$description]: $cmd")
             start()
             schedulers.add(this)
         }
